d02984aa200c97aa9fb8b12abdc8691ae57f57c6
Ian Scott <scottianw@gmail.com>
2023-09-05 17:15:10 -0400

    Initial commit

---
+++ invenio_groups/ext.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+#
+# This file is part of the invenio-groups package.
+# Copyright (C) 2023, MESH Research.
+#
+# invenio-groups is free software; you can redistribute it
+# and/or modify it under the terms of the MIT License; see
+# LICENSE file for more details.
+
+"""Groups extension for Invenio.
+"""
